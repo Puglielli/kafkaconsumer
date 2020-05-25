1aa7c2ef9e4ed21f046c2bb141597f23a1d423e8
ppuglielli <puglielli14@gmail.com>
2020-05-25 18:19:16 -0300

    feat: add Repository

---
+++ src/main/java/br/com/projetoitau/kafkaconsumer/repository/HistoricoRepository.java
@@ -0,0 +1,17 @@
+package br.com.projetoitau.kafkaconsumer.repository;
+
+import br.com.projetoitau.kafkaconsumer.model.HistoricoVO;
+import org.springframework.data.cassandra.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface HistoricoRepository extends CrudRepository<HistoricoVO, String> {
+
+    @Query(value = "select * from historico where id = (:id)")
+    List<HistoricoVO> getHistoricoById(@Param("id") UUID id);
+}
