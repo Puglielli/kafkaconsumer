bf07ff959e9db08a902abedb338cd5b428267012
ppuglielli <puglielli14@gmail.com>
2020-05-25 18:18:32 -0300

    feat: add Kafkaconsumer

---
+++ src/main/java/br/com/projetoitau/kafkaconsumer/KafkaService/KafkaConsumer.java
@@ -0,0 +1,38 @@
+package br.com.projetoitau.kafkaconsumer.KafkaService;
+
+import br.com.projetoitau.kafkaconsumer.model.HistoricoVO;
+import br.com.projetoitau.kafkaconsumer.repository.HistoricoRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableKafka
+@KafkaListener(topics = "bank-listener")
+public class KafkaConsumer {
+
+    @Autowired
+    HistoricoRepository historicoRepository;
+
+    @KafkaHandler
+    public void consume(@Payload String obj) throws JsonProcessingException {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        HistoricoVO historicoVO = mapper.readValue(obj, HistoricoVO.class);
+
+        if (!historicoRepository.getHistoricoById(historicoVO.getId()).isEmpty()) {
+            return ;
+        }
+
+        historicoRepository.save(historicoVO);
+
+        System.out.println("Historico do Producer: " + obj);
+        System.out.println("Historico do Consumer: " + historicoVO);
+    }
+}
