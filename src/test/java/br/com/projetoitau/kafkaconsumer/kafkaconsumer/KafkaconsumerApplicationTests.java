d1676584725c21ae18c47bbb78bcfdf0fe1c541b
ppuglielli <puglielli14@gmail.com>
2020-05-25 15:45:41 -0300

    feat: add new class

---
+++ src/test/java/br/com/projetoitau/kafkaconsumer/kafkaconsumer/KafkaconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoitau.kafkaconsumer.kafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
